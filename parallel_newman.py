581ed50baae3d96e02f9644d20cfacd1c18cc142
singh-anamika <50858869+singh-anamika@users.noreply.github.com>
2023-02-27 17:24:27 +0530

    Update parallel_newman.py

--- parallel_newman.py
+++ parallel_newman.py
@@ -7,7 +7,7 @@ with open('CVC_Apis.postman_collection.json', 'r') as f:
 
 random.shuffle(collection['item'])
 
-chunks = [collection['item'][i:i+10] for i in range(0, len(collection['item']), 10)]
+chunks = [collection['item'][i:i+10] for i in range(0, len(collection['item']), 5)]
 
 for i, chunk in enumerate(chunks):
     command = f"newman run CVC_Apis.postman_collection.json --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export report.xml"
