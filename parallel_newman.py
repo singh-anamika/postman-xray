42c97eea5bd9a7f2974bc96d094cb1e29a05c143
singh-anamika <50858869+singh-anamika@users.noreply.github.com>
2023-03-09 15:46:18 +0530

    Update parallel_newman.py

--- parallel_newman.py
+++ parallel_newman.py
@@ -10,7 +10,7 @@ random.shuffle(collection['item'])
 chunks = [collection['item'][i:i+10] for i in range(0, len(collection['item']), 20)]
 
 for i, chunk in enumerate(chunks):
-    command = f"newman run https://api.postman.com/collections/25156296-4a3d8161-02a7-4fd4-946e-03b0e9a2be5f?access_key=PMAT-01GTXS776P4X8A9WFZHV4DS293 --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export report.xml"
+    command = f"newman run https://api.postman.com/collections/18481634-89b56908-3278-40f6-aa7a-180089abbd04?access_key=PMAT-01GV2XX2ZTS8JS348HXSYS3E47 --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export report.xml"
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
 
